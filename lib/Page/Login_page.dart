93e25302f907ab496f3bc894a97e72934d59ce93
Bahaa-eddine-MB <bm.bouzeboudja@esi-sba.dz>
2022-03-18 18:04:26 +0100

    Loging logout Pages for DocTell

---
+++ lib/Page/Login_page.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Page/profile_page.dart';
+import 'package:flutter_application_1/common/them_helper.dart';
+import 'Registration_page.dart';
+import 'Widgets/Header_widget.dart';
+import 'forgot_password_page.dart';
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  double _headerHeight = 250;
+  final _formKey = GlobalKey<FormState>();
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: _headerHeight,
+              child: HeaderWidget(_headerHeight, true, Icons.login_rounded),
+            ),
+            SafeArea(
+              child: Container(
+                  padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  margin: EdgeInsets.fromLTRB(20, 10, 20, 10),
+                  //The login form
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        child: Image(
+                          image: AssetImage("img/LogingLogo.png"),
+                        ),
+                      ),
+                      Text(
+                        "Sign into your account",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      // Text(
+                      //   "DocTell",
+                      //   style: TextStyle(fontSize: 60, fontWeight: FontWeight.bold),
+                      // ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Form(
+                          key: _formKey,
+                          child: Column(
+                            children: [
+                              TextFormField(
+                                
+                                decoration: ThemHelper().textInputDecoration(
+                                    'User Name', 'Enter your user name'),
+                              validator: (val) {
+                              if (val!.isEmpty) {
+                                return "Please enter your user name";
+                              }
+                              return null;
+                            },
+                                    
+                                
+                              ),
+                              SizedBox(
+                                height: 30,
+                              ),
+                              TextFormField(
+                                obscureText: true,
+                                decoration: ThemHelper().textInputDecoration(
+                                    'Password', 'Enter your password'),
+                                    validator: (val) {
+                              if (val!.isEmpty) {
+                                return "Please enter your password";
+                              }
+                              return null;
+                            },
+                                
+                              ),
+                              SizedBox(
+                                height: 15,
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 0, 10, 20),
+                                alignment: Alignment.topRight,
+                                child: GestureDetector(
+                                  onTap: () {
+                                  Navigator.push( context, MaterialPageRoute( builder: (context) => ForgotPasswordPage()), 
+                                  );},
+                                  child: Text( "Forgot your password?", style: TextStyle( color: Colors.grey, ),
+                                ),
+                                
+                                ),
+                                
+                              ),
+                              Container(
+                                decoration:
+                                    ThemHelper().buttonBoxDecoration(context),
+                                child: ElevatedButton(
+                                  style: ThemHelper().buttonStyle(),
+                                  child: Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(40, 10, 40, 10),
+                                    child: Text(
+                                      'sign in'.toUpperCase(),
+                                      style: TextStyle(
+                                          fontSize: 20,
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white),
+                                    ),
+                                  ),
+                                  onPressed: () {
+                                   if (_formKey.currentState!.validate()) {
+                                Navigator.of(context).pushAndRemoveUntil(
+                                    MaterialPageRoute(
+                                        builder: (context) => ProfilePage()
+                                    ),
+                                        (Route<dynamic> route) => false
+                                );
+                              }
+                                  },
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                                //  child:Text("Don't have an account? Create!"),
+                                child: Text.rich(TextSpan(children: [
+                                  TextSpan(text: "Don't have an account?"),
+                                  TextSpan(
+                                    text: "Create!",
+                                    recognizer: TapGestureRecognizer()
+                                      ..onTap = () {
+                                        Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                                builder: (context) =>
+                                                    RegistrationPage()));
+                                      },
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .secondary),
+                                  ),
+                                ])),
+                              )
+                            ],
+                          ))
+                    ],
+                  )),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  buttonBoxDecoration(BuildContext context) {}
+
+  forgotPasswordPage() {}
+}
---
+++ lib/Page/profile_page.dart
@@ -0,0 +1,362 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_application_1/Page/Login_page.dart';
+import 'package:flutter_application_1/Page/Registration_page.dart';
+import 'package:flutter_application_1/Page/SplashScreen.dart';
+import 'package:flutter_application_1/Page/Widgets/Header_widget.dart';
+import 'package:flutter_application_1/Page/forgot_password_page.dart';
+import 'package:flutter_application_1/Page/verification_page.dart';
+
+class ProfilePage extends StatefulWidget {
+  ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  double _drawerIconSize = 24;
+  double _drawerFontSize = 17;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Profile page",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        elevation: 0.5,
+        iconTheme: IconThemeData(color: Colors.white),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: <Color>[
+                Theme.of(context).primaryColor,
+                Theme.of(context).colorScheme.secondary
+              ])),
+        ),
+        actions: [
+          Container(
+            margin: EdgeInsets.only(top: 16, right: 16),
+            child: Stack(
+              children: <Widget>[
+                Icon(Icons.notifications),
+                Positioned(
+                  right: 0,
+                  child: Container(
+                    padding: EdgeInsets.all(1),
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                    constraints: BoxConstraints(
+                      minWidth: 12,
+                      minHeight: 12,
+                    ),
+                    child: Text(
+                      "0",
+                      style: TextStyle(color: Colors.white, fontSize: 8),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+      drawer: Drawer(
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0.0, 1.0],
+            colors: [
+              Theme.of(context).primaryColor.withOpacity(0.1),
+              Theme.of(context).colorScheme.secondary.withOpacity(0.4),
+            ],
+          )),
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    stops: [0.0, 1.0],
+                    colors: [
+                      Theme.of(context).primaryColor,
+                      Theme.of(context).colorScheme.secondary,
+                    ],
+                  ),
+                ),
+                child: Container(
+                  alignment: Alignment.bottomLeft,
+                  child: Text(
+                    "Doctell",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.screen_lock_landscape_rounded,
+                  size: _drawerIconSize,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text(
+                  "Splash screen",
+                  style: TextStyle(
+                      fontSize: 17, color: Theme.of(context).primaryColor),
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => SplashScreen(
+                                title: "Splash Screen",
+                              )));
+                },
+              ),
+              Divider(
+                color: Theme.of(context).primaryColor,
+                height: 1,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.login_rounded,
+                  size: _drawerIconSize,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text(
+                  "Login Page",
+                  style: TextStyle(
+                      fontSize: 17, color: Theme.of(context).primaryColor),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => LoginPage()));
+                },
+              ),
+              Divider(
+                color: Theme.of(context).primaryColor,
+                height: 1,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.person_add_alt_1,
+                  size: _drawerIconSize,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text(
+                  "Registration Page",
+                  style: TextStyle(
+                      fontSize: 17, color: Theme.of(context).primaryColor),
+                ),
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => RegistrationPage()));
+                },
+              ),
+              Divider(
+                color: Theme.of(context).primaryColor,
+                height: 1,
+              ),
+              ListTile(
+                 leading: Icon(
+                  Icons.password_rounded,
+                  size: _drawerIconSize,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text(
+                  "Forgot Password Page",
+                  style: TextStyle(
+                      fontSize: 17, color: Theme.of(context).primaryColor),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => ForgotPasswordPage()));
+                },    
+              ),
+              Divider(
+                color: Theme.of(context).primaryColor,
+                height: 1,
+              ),
+              ListTile(
+                 leading: Icon(
+                  Icons.verified_user_sharp,
+                  size: _drawerIconSize,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text(
+                  "Verification Page",
+                  style: TextStyle(
+                      fontSize: 17, color: Theme.of(context).primaryColor),
+                ),
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => VerificationPage()));
+                },
+                
+              ),
+              Divider(
+                color: Theme.of(context).primaryColor,
+                height: 1,
+              ),
+              ListTile(
+                leading: Icon(
+                  Icons.logout_rounded,
+                  size: _drawerIconSize,
+                  color: Theme.of(context).primaryColor,
+                ),
+                title: Text(
+                  "Logout",
+                  style: TextStyle(
+                      fontSize: 17, color: Theme.of(context).primaryColor),
+                ),
+                onTap: () {
+                  SystemNavigator.pop();
+                },
+                
+              ),
+              Divider(
+                color: Theme.of(context).primaryColor,
+                height: 1,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Stack(
+          children: [
+            Container(
+              height: 100,
+              child: HeaderWidget(100, false, Icons.house_rounded),
+            ),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.fromLTRB(25, 10, 25, 10),
+              padding: EdgeInsets.fromLTRB(10, 0, 10, 0),
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(100),
+                        border: Border.all(width: 5, color: Colors.white),
+                        color: Colors.white,
+                        boxShadow: [
+                          BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 20,
+                              offset: const Offset(5, 5)),
+                        ]),
+                    child: Icon(
+                      Icons.person,
+                      size: 80,
+                      color: Colors.grey.shade400,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Bahaa Eddine",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "A doctor",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          padding:
+                              const EdgeInsets.only(left: 8.0, bottom: 4.0),
+                          alignment: Alignment.topLeft,
+                          child: Text(
+                            "User Information",
+                            style: TextStyle(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.w500,
+                              fontSize: 16,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Card(
+                          child: Container(
+                            alignment: Alignment.topLeft,
+                            padding: EdgeInsets.all(15),
+                            child: Column(
+                              children: <Widget>[
+                                Column(
+                                  children: <Widget>[
+                                    ...ListTile.divideTiles(
+                                      color: Colors.grey,
+                                      tiles: [
+                                        ListTile(
+                                          contentPadding: EdgeInsets.symmetric(
+                                              horizontal: 12, vertical: 4),
+                                          leading: Icon(Icons.my_location),
+                                          title: Text("Location"),
+                                          subtitle: Text("Algeria"),
+                                        ),
+                                        ListTile(
+                                          leading: Icon(Icons.email),
+                                          title: Text("Email"),
+                                          subtitle:
+                                              Text("bm.bouzeboudja@esi-sba.dz"),
+                                        ),
+                                        ListTile(
+                                          leading: Icon(Icons.phone),
+                                          title: Text("Phone"),
+                                          subtitle: Text("246-6421-52"),
+                                        ),
+                                        ListTile(
+                                          leading: Icon(Icons.person),
+                                          title: Text("About Me"),
+                                          subtitle: Text(
+                                              "This is a about me link and you can khow about me in this section."),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  forgotPasswordPage() {}
+}
